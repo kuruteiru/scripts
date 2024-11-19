05c9b456b3660db1bb0c448d80cff22001fefc9c
kuruteiru <martin.krutil@email.cz>
2024-11-19 09:48:41 +0100

    fixed -s, added -ls

--- chaud.sh
+++ chaud.sh
@@ -10,6 +10,11 @@ elif [[ $1 == "-h" ]]; then
 elif [[ $1 == "-s" ]]; then
 	echo "audio output changed to speakers"
     for stream in $(pactl list short sink-inputs | awk '{print $1}'); do 
-            pactl move-sink-input $stream "alsa_output.pci-0000_2d_00.4.analog-stereo"; 
+            pactl move-sink-input $stream "alsa_output.usb-Lenovo_ThinkPad_Thunderbolt_4_Dock_USB_Audio_000000000000-00.analog-stereo"
+    done
+elif [[ $1 == "-ls" ]]; then
+	echo "audio output changed to laptop speakers"
+    for stream in $(pactl list short sink-inputs | awk '{print $1}'); do 
+            pactl move-sink-input $stream "alsa_output.pci-0000_c3_00.6.HiFi__Speaker__sink"
     done
 fi
