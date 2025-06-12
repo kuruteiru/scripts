ce28063053ab085d34fac9810ec948911305b907
kuruteiru <martin.krutil@email.cz>
2025-06-12 11:48:46 +0200

    kdialog

--- screenshot.sh
+++ screenshot.sh
@@ -1,10 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 AREA=$(slurp)
-[ -z "$AREA" ] && exit 1
+if [ -z "$AREA" ]; then
+  echo "No area selected. Exiting."
+  exit 1
+fi
 
 DEFAULT_NAME="screenshot_$(date '+%d-%m-%Y_%H:%M:%S').png"
-FILE=$(zenity --file-selection --save --confirm-overwrite --filename="$HOME/images/screenshots/$DEFAULT_NAME")
-[ -z "$FILE" ] && exit 1
+DEFAULT_PATH="$HOME/images/screenshots/$DEFAULT_NAME"
+
+mkdir -p "$HOME/images/screenshots"
+
+FILE=$(kdialog --getsavefilename "$DEFAULT_PATH" "*.png")
+if [ -z "$FILE" ]; then
+  echo "No file selected. Exiting."
+  exit 1
+fi
 
 grim -g "$AREA" "$FILE"
